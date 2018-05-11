e6e1b404df3351fb06476d48cbfbf8a89f47e2aa
aep@intimap.com <aep>
2018-05-11 16:35:57 +0700

    set preventDefault img thumbnail

--- asset/js/portfolio.js
+++ asset/js/portfolio.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
 	$('.page-scroll').on('click', function (event) {
 		event.preventDefault();
-		
+
 		var target = $(this).attr('href');
 		var elemTarget = $(target);
 
 		$('body').animate({
 			scrollTop: elemTarget.offset().top - 50
 		}, 1000, 'easeInOutExpo');
-	});		
+	});
+
+	$('.img-portfolio a').each(function (i, a) {
+		$(a).bind('click', function (e) {
+			e.preventDefault();
+		});
+	});
 });
